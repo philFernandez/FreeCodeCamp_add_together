e9f94775626f9254c9894dee06540a2adb16de22
Phil Fernandez <norcal618@live.com>
2017-03-20 23:20:26 -0700

    done

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('shit')
+function addTogether(x, y) {	
+
+	for (i = 0; i < arguments.length; i++) {
+		if (typeof arguments[i] !== 'number') {
+			return undefined;
+		}
+	}
+
+	if (arguments.length === 2) {
+		return x + y;
+	}
+	else {
+		return function(z) {
+			if (typeof arguments[0] !== 'number') {
+				return undefined;
+			}
+			return x + z;
+		};
+	}	
+
+}
+
+console.log(addTogether(2)(3));
